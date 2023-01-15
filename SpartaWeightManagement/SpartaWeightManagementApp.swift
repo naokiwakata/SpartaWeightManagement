97641ecb3b9cd599d933f41a70f02735fad9f68e
naokiwakata <rockwakata15@gmail.com>
2023-01-15 22:08:11 +0900

    Firebaseの導入

--- SpartaWeightManagement/SpartaWeightManagementApp.swift
+++ SpartaWeightManagement/SpartaWeightManagementApp.swift
@@ -1,17 +1,27 @@
-//
-//  SpartaWeightManagementApp.swift
-//  SpartaWeightManagement
-//
-//  Created by 若田直希 on 2023/01/15.
-//
-
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
-struct SpartaWeightManagementApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
