3772e875c125fa93c240cd46cdf382e92b62ae4a
naokiwakata <rockwakata15@gmail.com>
2023-01-15 21:25:05 +0900

    Initial Commit

---
+++ SpartaWeightManagement/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpartaWeightManagement
+//
+//  Created by 若田直希 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpartaWeightManagement/SpartaWeightManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpartaWeightManagementApp.swift
+//  SpartaWeightManagement
+//
+//  Created by 若田直希 on 2023/01/15.
+//
+
+import SwiftUI
+
+@main
+struct SpartaWeightManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
